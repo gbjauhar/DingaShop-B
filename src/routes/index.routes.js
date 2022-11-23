e4767eae7d0338ae146e2b1d171da66c37a1e756
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-23 09:12:31 -0300

    refactor: change name path auth to user

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { registerClient, loginClient } from '../controllers/auth.controllers.js';
-import { validateBodyRegister, validateLogin, validateNewRegister } from '../middlewares/auth.middlewares.js';
+import { validateBodyRegister, validateLogin, validateNewRegister } from '../middlewares/user.middlewares.js';
 
 const routes = express.Router();
 
