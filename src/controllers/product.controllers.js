bb175b2ac1155f1b845294a41a4306cca1bd5153
Gabriela Jauhar <gbjauhar@gmail.com>
2022-11-23 18:19:48 -0300

    feat: create product schema and post

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,11 +1,23 @@
 import { productsCollection } from '../database/index.js';
 
-export default async function getCatalog(req, res) {
+export async function getCatalog(req, res) {
   try {
     const catalog = await productsCollection.find({}).toArray();
 
     return res.status(200).send({ catalog });
   } catch (err) {
     return res.status(500).send({ error: err });
+  }
+}
+
+export async function createProduct(req, res) {
+  const { product } = res.locals;
+  console.log(product);
+  try {
+    await productsCollection.insertOne({ product });
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
   }
 }
